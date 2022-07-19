9d11de0237ae745a365de1345636763597f03c53
Petrisor Bogdan <bogdan.petrisor@yopeso.com>
2022-07-19 15:35:58 +0300

    Create a desertTimer() and start it in onStart() callback and stop it in onStop() callback

--- DessertClickerLogs/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ DessertClickerLogs/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -32,6 +32,7 @@ class MainActivity : AppCompatActivity() {
 
     private var revenue = 0
     private var dessertsSold = 0
+    private lateinit var dessertTimer: DessertTimer
 
     // Contains all the views
     private lateinit var binding: ActivityMainBinding
@@ -74,6 +75,7 @@ class MainActivity : AppCompatActivity() {
         binding.dessertButton.setOnClickListener {
             onDessertClicked()
         }
+        dessertTimer= DessertTimer()
 
         // Set the TextViews to the right values
         binding.revenue = revenue
@@ -153,7 +155,7 @@ class MainActivity : AppCompatActivity() {
     /** Lifecycle Methods **/
     override fun onStart() {
         super.onStart()
-
+        dessertTimer.startTimer()
         Timber.i("onStart called")
     }
 
@@ -169,6 +171,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStop() {
         super.onStop()
+        dessertTimer.stopTimer()
         Timber.i("onStop Called")
     }
 
